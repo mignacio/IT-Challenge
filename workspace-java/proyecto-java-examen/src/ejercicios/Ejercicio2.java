babb1fd486279d8d5298dd8b55df8968ffcc5e61
Ignacio Moya <moyaignacioj@gmail.com>
2019-03-30 16:37:20 -0300

    java ejercicio 2 resuelto

--- workspace-java/proyecto-java-examen/src/ejercicios/Ejercicio2.java
+++ workspace-java/proyecto-java-examen/src/ejercicios/Ejercicio2.java
@@ -1,5 +1,9 @@
 package ejercicios;
 
+import ejercicios.Persona.tipoDoc;
+import java.time.LocalDate;
+import java.time.Month;
+
 /**
  * A. Crear una clase Persona con los siguientes campos
  * (con sus respectivos getters, setters y constructor)
@@ -34,8 +38,18 @@ public class Ejercicio2 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method stub			
+		
+		Persona persona = new Persona(tipoDoc.DNI, 35583274, "Ignacio", "Moya", LocalDate.of(1991, Month.JANUARY,  17));
+		
+		System.out.println("Tipo de Documento: "+ persona.getTipoDocumento().toString());
+		System.out.println("Nro de Documento: "+ persona.getNroDocumento().toString());
+		System.out.println("Nombre: "+ persona.getNombre().toString());
+		System.out.println("Apellido: "+ persona.getApellido().toString());
+		System.out.println("Fecha de nacimiento: "+ persona.getFechaNacimiento().toString());
+		
 	}
 
 }
+
+
