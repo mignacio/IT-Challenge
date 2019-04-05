70533a60209b68eed3a1b5f11cb802387791c37b
Ignacio Moya <moyaignacioj@gmail.com>
2019-04-05 15:00:32 -0300

    comentarios y emprolijamiento

--- ej3/defines.php
+++ ej3/defines.php
@@ -1,20 +1,11 @@
 <?php
 
-/*
-$db_host = getenv('JAWSDB_HOST');
-$db_port = getenv('JAWSDB_PORT');
-$db_user = getenv('JAWSDB_USERNAME');
-$db_databse = getenv('JAWSDB_DATABASE');
-$db_password = getenv('JAWSDB_PASS');
-*/
-
 define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
 define('DB_USER', 'root');
 define('DB_DATABASE', 'it-challenge');
 define('DB_PASS', '');
 
-//$mysqli = mysqli_connect($db_host . ":" . $db_port, $db_user, $db_password, $db_database);
 $mysqli = mysqli_connect(DB_HOST . ":" . DB_PORT, DB_USER, DB_PASS, DB_DATABASE);
 if (mysqli_connect_errno($mysqli)) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
--- ej3/dibujarFilas.php
+++ ej3/dibujarFilas.php
@@ -1,4 +1,9 @@
 <?php
+/**
+* @params $result espera un resultado de una consulta sql que devuelva filas
+* dibuja en html cada una de las filas del resultado de la consulta
+*/
+
 function dibujarFilas($result) {
   while ($row=mysqli_fetch_row($result))
   {
